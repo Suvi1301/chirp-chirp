9cf95fcd5d5ded4761cabbbc33c5b2e1e8ef3589
Suvineet Singh <suvineet1998@gmail.com>
2019-11-10 01:03:21 +0000

    [#7] Generate a Spectrogram from WAV file.

--- src/helpers/audio_helper.py
+++ src/helpers/audio_helper.py
@@ -1,11 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import pydub
 import struct
-import scipy.io.wavfile
+import scipy.io.wavfile as wavfile
 from enum import Enum
 
 
 raw_file_path = "./../data/raw/"
 processed_file_path = "./../data/processed/"
+spectrograms_file_path = "./../data/spectrograms/"
 
 
 class AudioFormat(Enum):
@@ -32,3 +35,35 @@ def read_file(filename: str, format=AudioFormat.MP3):
 def convert_wav(filename: str):
     file = read_file(filename, AudioFormat.MP3)
     file.export(f'{processed_file_path}{filename}.wav', format="wav")
+
+
+def read_wav(filename: str):
+    ''' Read processed file and return the audio data to be plotted. '''
+    sampling_freq, signal_data = wavfile.read(
+        f'{processed_file_path}{filename}.wav'
+    )
+    try:
+        if len(signal_data.shape) > 1:
+            single_channel_data = signal_data[:, 0]
+        else:
+            single_channel_data = signal_data
+    except IndexError as ex:
+        print(
+            f'Error reading single channel from filename: {filename}. Reason="{ex}"'
+        )
+
+    return (sampling_freq, single_channel_data)
+
+
+def generate_spectrogram(
+    filename: str, nfft: int = 512, window=np.hamming(512)
+):
+    ''' Plot a spectrogram for audio file and save '''
+    sampling_freq, signal_data = read_wav(filename)
+    fig = plt.figure()
+    # TODO: Figure out overlap.
+    plt.specgram(signal_data, Fs=sampling_freq, NFFT=nfft, window=window)
+    # TODO: Modify plot to ignore axis labels etc.
+    plt.xlabel('Time')
+    plt.ylabel('Frequency')
+    fig.savefig(f'{spectrograms_file_path}{filename}.png')
